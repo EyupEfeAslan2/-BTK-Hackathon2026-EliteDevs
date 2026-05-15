9379be484ec58804ad063c8cea3fa96c7880e681
EyupEfeAslan2 <aslaneyupefe2@gmail.com>
2026-05-16 00:25:32 +0300

    Fix: Hardcoded Render URL for deployment.

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3030";;
+const API_BASE_URL = "https://btk-hackathon2026-elitedevs.onrender.com/"
 
 const client = axios.create({
   baseURL: API_BASE_URL,
